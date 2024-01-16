30b8349f2ae0345e5492647943fd5be4d9da1d32
Luis Brodely <luisbrodely@advantedigital.com>
2024-01-15 21:14:22 -0600

    sin interfaz

--- doc.py
+++ doc.py
@@ -1,14 +1,22 @@
 import random
 from math import log2
 
+# Declarar variables globales
+a = 0
+b = 0
+deltaX = 0
+poblacion_maxima = 0
+
 def generar_individuo(n):
     individuo = ''.join(random.choice('01') for _ in range(n))
     return individuo
 
 def operaciones_basicas():
+    global a, b, deltaX, poblacion_maxima  # Declarar como globales
     deltaX = float(input("Ingrese el valor de deltaX: "))
     a = float(input("Ingrese el valor de a: "))
     b = float(input("Ingrese el valor de b: "))
+    poblacion_maxima = int(input("Ingrese el valor de la población máxima: "))
 
     if deltaX <= 0 or a >= b:
         print("Error: Verifique que deltaX sea positivo y que 'a' sea menor que 'b'.")
@@ -25,14 +33,14 @@ def operaciones_basicas():
     return int(n)
 
 def generar_poblacion():
-    poblacion_maxima = int(input("Ingrese el valor de la población máxima: "))
     n = operaciones_basicas()
     poblacion = []
 
     for _ in range(poblacion_maxima):
         individuo = generar_individuo(n)
         valor_entero = int(individuo, 2)
-        poblacion.append([individuo, valor_entero])
+        x = a + valor_entero * deltaX  # Calcula el valor de x
+        poblacion.append([individuo, valor_entero, x])
 
     return poblacion
 
@@ -40,6 +48,6 @@ def generar_poblacion():
 poblacion_resultados = generar_poblacion()
 
 # Imprimir la tabla
-print("Individuo, Valor Entero")
-for individuo, individuo_entero in poblacion_resultados:
-    print(f"{individuo}, {individuo_entero}")
+print("Individuo, Valor Entero, x")
+for individuo, valor_entero, x in poblacion_resultados:
+    print(f"{individuo}, {valor_entero}, {x}")
